8b6b2dfe3fbcd99fbb27512405198c08bedb7af4
dshinzie <dshin@rivt.com>
2020-10-16 13:13:45 -0500

    change account to user

--- starterkit/starterkit/settings.py
+++ starterkit/starterkit/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -56,8 +58,8 @@ INSTALLED_APPS = [
     'corsheaders',
     # App specifiic
     'starterkit',
-    'account.apps.AccountConfig',
-    'friendship'
+    'friendship',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -96,8 +98,11 @@ WSGI_APPLICATION = 'starterkit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
     }
 }
 
@@ -120,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'starterkit.User'
+AUTH_USER_MODEL = 'user.User'
 
 
 # Internationalization
@@ -141,3 +146,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Celery and Redis
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+# CORS Headers
+CORS_ALLOW_HEADERS = default_headers + (
+   'Api-Key',
+)
+
+# Authentication
+ACCOUNT_EMAIL_VERIFICATION = None
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = False
+
+# API Keys
+API_KEY_CUSTOM_HEADER = 'Api-Key'
--- starterkit/starterkit/urls.py
+++ starterkit/starterkit/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('auth/', include('auth.urls')),
-    # path('account/', include('account.urls')),
+    # path('user/', include('user.urls')),
     # path('friendship/', include('friendship.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
